762b6fdefe89903e80cddcc66d5da60d0295f1a0
Yoonsung <lvev9925@naver.com>
2015-02-04 16:29:23 +0900

    OneToOne Relation Test

--- tutorialPointPractice/src/com/architree/eclipselink/entity/Employee.java
+++ tutorialPointPractice/src/com/architree/eclipselink/entity/Employee.java
@@ -4,8 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Employee {
@@ -16,7 +15,7 @@ public class Employee {
 	private double salary;
 	private String deg;
 
-	@ManyToOne
+	@OneToOne
 	private Department department;
 	
 	public Department getDepartment() {
